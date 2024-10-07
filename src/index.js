d7b552c37b3bbc3c7455db14ca4dbcca7054fcd9
KariFigz <karanda1319@gmail.com>
2024-10-06 17:29:56 -0700

    pushing to gh

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 // TODO: Add the missing query selectors:
-const score = document.querySelector('.score'); // Use querySelector() to get the score element
-const timerDisplay = document.querySelector('.timer'); // use querySelector() to get the timer element.
+const score = document.querySelector('#score'); // Use querySelector() to get the score element
+const timerDisplay = document.querySelector('#timer'); // use querySelector() to get the timer element.
 
 let time = 0;
 let timer;
@@ -169,7 +169,7 @@ function updateScore() {
   // Increment the points variable by 1
   points++;
 // Update the scoreboard with the new points value
-  let score = document.getElementById("scoreboard");
+  let score = document.getElementById("score");
   if (score) {
     score.textContent = points;
   } 
